a94725d89a8ffa2b3aa02ed3f1ca0754ef7d3373
RifatMahmudno-1 <rifatmahmudpc@gmail.com>
2026-06-22 20:34:30 +0600

    clear terminal scrollback buffer too

--- main.go
+++ main.go
@@ -95,7 +95,10 @@ func getCommandsPath() string {
 }
 
 func clearConsole() {
-	fmt.Print("\033[H\033[2J")
+	// \033[H: Move to top
+	// \033[2J: Clear screen
+	// \033[3J: Clear scrollback buffer
+	fmt.Print("\033[H\033[2J\033[3J")
 }
 
 func confirmExit() {
